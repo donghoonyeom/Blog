6c61be956c496bbb04f11423d447e0e40c5c065e
donghoonyeom <dua5410773@naver.com>
2023-12-15 01:26:24 +0900

    First commit

---
+++ blog-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "hoon.blog",
+  description: "꾸준히, 의미있는 학습을 기록하기 위한 공간입니다.",
+  author: "donghoonyeom",
+  siteUrl: "https://hooblog.netlify.app",
+  links: {
+    github: "https://github.com/donghoonyeom",
+    facebook: "https://www.facebook.com/profile.php?id=100005481754856",
+    email: "mailto:dua541541@gmail.com"
+  },
+  utterances: {
+    repo: "donghoonyeom/blog",
+    type: "pathname",
+  },
+}
+

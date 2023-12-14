fc957e75459833a73aab195ab0a256075ae328ed
donghoonyeom <dua5410773@naver.com>
2023-12-15 06:41:35 +0900

    블로그 주소 변경

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "hoon.blog",
   description: "꾸준히, 의미있는 학습을 기록하기 위한 공간입니다.",
   author: "donghoonyeom",
-  siteUrl: "https://hoonblog.netlify.app",
+  siteUrl: "https://hoonblog.netlify.app/",
   links: {
     github: "https://github.com/donghoonyeom",
     facebook: "https://www.facebook.com/profile.php?id=100005481754856",
